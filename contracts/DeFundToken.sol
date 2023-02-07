580d2111e3f2adc515f86c60c66851d567224d64
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-07 14:25:54 +0530

    Create basic ERC20 token for txns on crowdfunding platform

---
+++ contracts/DeFundToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GNU
+
+pragma solidity ^0.8.5;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DeFundToken is ERC20, Ownable {
+
+    uint256 initialSupply;
+    constructor (uint256 _initialSupply) ERC20("DeFundToken", "DFND") {
+        initialSupply = _initialSupply;
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount);
+    }
+
+}
