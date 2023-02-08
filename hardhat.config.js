3b7da922834b73c872a22e29b189f0e272b40083
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-08 23:26:39 +0530

    Create first unit test - Reinstalled all dependecies - import hardhat-ethers in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
 };
--- test/defund.test.js
+++ test/defund.test.js
@@ -5,3 +5,46 @@ const { BigNumber } = require("ethers");
 
 const expect = chai.expect;
 chai.use(chaiAsPromised);
+
+let deployer, creator, donor1, donor2, addrs;
+let crowdFunding, dfnd;
+
+const campaignParams = [100, Math.floor(Date.now() / 1000) + 100, "SampleURI"];
+const dfndInitialSupply = 10000000;
+
+before(async function () {
+  [deployer, creator, donor1, donor2, ...addrs] = await ethers.getSigners();
+
+  const addresses = [creator, donor1, donor2, ...addrs].map(
+    (signer) => signer.address
+  );
+
+  const DFND = await ethers.getContractFactory("DeFundToken");
+  dfnd = await DFND.connect(deployer).deploy(dfndInitialSupply);
+  await dfnd.deployed();
+
+  for (let address of addresses) {
+    await dfnd.connect(deployer).transfer(address, 10000);
+  }
+
+  const CrowdFunding = await ethers.getContractFactory("CrowdFunding");
+  crowdFunding = await CrowdFunding.connect(deployer).deploy(dfnd.address);
+  await crowdFunding.deployed();
+});
+
+describe("I. Creating a campaign", () => {
+  it("1. Users should be able to create campaigns with custom specifications", async () => {
+    const campaignId = await crowdFunding.callStatic.createCampaign(
+      creator.address,
+      ...campaignParams
+    );
+
+    await expect(
+      crowdFunding
+        .connect(creator)
+        .createCampaign(creator.address, ...campaignParams)
+    ).to.eventually.be.fulfilled;
+
+    expect(await crowdFunding.getCampaignById(campaignId));
+  });
+});
