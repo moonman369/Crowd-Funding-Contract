f98afd504e6eb48a98b497ec1cb613ee3784625d
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-11 01:57:14 +0530

    Changes to deploy scripts - Add proper messages - Add prompt package as dep to take value from terminal - Added prompt get to upgrade-proxy

--- scripts/deploy-functions.js
+++ scripts/deploy-functions.js
@@ -5,46 +5,50 @@ const initialSupply = 1000000000;
 const deployDFND = async (deployerAddress, initialSupply) => {
   const deploySigner = await ethers.getSigner(deployerAddress);
   const DFND = await ethers.getContractFactory("DeFundToken");
+  console.log("Deploying DFND Contract...");
   const dfnd = await DFND.connect(deploySigner).deploy(initialSupply);
   await dfnd.deployed();
-  console.log(`DFND token contract was deployed at address: ${dfnd.address}`);
+  console.log(
+    `DFND token contract was successfully deployed at address: ${dfnd.address}\n`
+  );
   return dfnd;
 };
 
 const deployCrowdFunding = async (deployerAddress, dfndTokenAddress) => {
   const deploySigner = await ethers.getSigner(deployerAddress);
   const CrowdFunding = await ethers.getContractFactory("CrowdFunding");
+  console.log("Deploying CrowdFunding Contract...");
   const crowdFunding = await CrowdFunding.connect(deploySigner).deploy(
     dfndTokenAddress
   );
   await crowdFunding.deployed();
   console.log(
-    `CrowdFunding contract was deployed at address: ${crowdFunding.address}`
+    `CrowdFunding contract was successfully deployed at address: ${crowdFunding.address}\n`
   );
   return crowdFunding;
 };
 
 const deployCrowdFundingProxy = async (dfndTokenAddress) => {
   const CFProxy = await ethers.getContractFactory("CrowdFundingUpgradableV1");
+  console.log("Deploying CrowdFundingUpgradableV1 proxy contract...");
   const cfProxy = await upgrades.deployProxy(CFProxy, [dfndTokenAddress], {
     initializer: "setTokenAddress",
   });
-
   await cfProxy.deployed();
-
-  console.log(`CrowdFundingProxy was deployed at address: ${cfProxy.address}`);
-
-  return cfProxy.address;
+  console.log(
+    `CrowdFundingUpgradableV1 proxy was deployed at address: ${cfProxy.address}\n`
+  );
+  return cfProxy;
 };
 
 const upgradeCrowdFundingProxy = async (proxyAddress) => {
   const CFProxyV2 = await ethers.getContractFactory("CrowdFundingUpgradableV2");
-
+  console.log("Upgrading CrowdFundingUpgradableV1 proxy...");
   const cfProxyV2 = await upgrades.upgradeProxy(proxyAddress, CFProxyV2);
-
-  console.log("Upgrade Successful");
-
-  return cfProxyV2.address;
+  console.log(
+    `CrowdFundingUpgradableV1 proxy contract at address ${proxyAddress} was successfully upgraded to CrowdFundingUpgradableV2\n`
+  );
+  return cfProxyV2;
 };
 
 module.exports = {
--- scripts/upgrade-proxy.js
+++ scripts/upgrade-proxy.js
@@ -1,12 +1,29 @@
 const { ethers } = require("hardhat");
+const prompt = require("prompt");
 const { upgradeCrowdFundingProxy } = require("./deploy-functions");
 
-const main = async () => {
-  await upgradeCrowdFundingProxy("0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9");
-};
+// const main = async () => {
 
-main()
-  .catch((error) => console.error(error))
-  .then(() => {
-    process.exit(0);
-  });
+//   const cf2 = await upgradeCrowdFundingProxy(
+//     "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+//   );
+// };
+
+// main()
+//   .catch((error) => console.error(error))
+//   .then(() => {
+//     process.exit(0);
+//   });
+
+prompt.start();
+
+console.log("Please enter valid Crowdfunding Proxy Contract Address...");
+
+prompt.get(["proxyAddress"], async (error, result) => {
+  const proxy = result.proxyAddress.toString();
+  if (ethers.utils.isAddress(proxy)) {
+    await upgradeCrowdFundingProxy(proxy);
+    return;
+  }
+  console.log("Invalid Proxy Address! Try again!");
+});
